8743724ad94bda5556d77d47f7e8611d7cf71a1a
surfing1231 <surfing.enthusiast@gmail.com>
2016-05-08 18:52:58 +0800

    udpate code structure

--- Localization/formats.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  _format:{},
-  add: function(lang, formatsArray){
-    this._format[lang] = formatsArray;
-  },
-  get: function(lang){
-    return this._format[lang];
-  }
-}
--- setup/0_env_setup.js
+++
@@ -1,8 +0,0 @@
-var jsdom = require('jsdom');
-
-process.browser = true;
-global.document = jsdom.jsdom();
-global.window = document.parentWindow;
-global.navigator = {
-	userAgent: 'node.js'
-};
